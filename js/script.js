a2bbc273ac09747c23c55b2907cbc70b3db141f1
Elyse Masterson <elyselani@gmail.com>
2017-04-10 16:14:58 -0700

    week 1 homework

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var numbers = [];
+
+function addNumber () {
+  var funNumber = Number(document.addForm.number.value);
+  if (!isNaN(funNumber)){
+    numbers.push(funNumber);
+    updatePage();
+    document.addForm.number.value = "";
+  }
+  else {
+    alert("Oops! Looks like you didn't enter a number.  Only numbers, please!");
+  }
+  event.preventDefault();
+}
+
+function updatePage () {
+  document.getElementById("count").innerHTML = numbers.length;
+  var sum = 0;
+  
+  for (var i = 0; i < numbers.length; i++){
+    sum = sum + numbers[i];
+  }
+  
+  document.getElementById("sum").innerHTML = sum;
+  
+  var avg = 0;
+  var totalNumbers = numbers.length;
+  avg = (sum / totalNumbers);
+  document.getElementById("average").innerHTML = avg;
+  
+  event.preventDefault();
+}
+
+function clearForm() {
+document.getElementById("addForm").reset();
+document.getElementById("count").innerHTML = "0";
+document.getElementById("sum").innerHTML = "0";
+document.getElementById("average").innerHTML = "0";
+numbers = [];
+event.preventDefault();
+}
