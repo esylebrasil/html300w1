7760c705797c4f59095f0ede88fd1138dca826e3
Elyse Masterson <elyselani@gmail.com>
2017-04-12 07:19:45 -0700

    Change error clearing and number reset

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function addNumber () {
   }
   else {
     alert("Oops! Looks like you didn't enter a number.  Only numbers, please!");
+    document.getElementById("addForm").reset(); 
   }
   event.preventDefault();
 }
@@ -25,6 +26,7 @@ function updatePage () {
   
   var avg = 0;
   var totalNumbers = numbers.length;
+  
   avg = (sum / totalNumbers);
   document.getElementById("average").innerHTML = avg;
   
@@ -32,10 +34,14 @@ function updatePage () {
 }
 
 function clearForm() {
-document.getElementById("addForm").reset();
-document.getElementById("count").innerHTML = "0";
-document.getElementById("sum").innerHTML = "0";
-document.getElementById("average").innerHTML = "0";
-numbers = [];
-event.preventDefault();
+  document.getElementById("addForm").reset();
+  document.getElementById("count").innerHTML = "0";
+  document.getElementById("sum").innerHTML = "0";
+  document.getElementById("average").innerHTML = "0";
+  
+  for (var i = numbers.length; i > 0; i--) {
+    numbers.pop(); 
+  }
+  
+  event.preventDefault();
 }
